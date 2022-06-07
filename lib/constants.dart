fa0ef30e1b6cda58360f99e293d7b326150fe9c5
clab21 <clab2144@gmail.com>
2022-06-07 12:57:00 +0700

    Movie Carousel

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//Colors that use in our app
+const kSecondaryColor = Color(0xFFFE6D8E);
+const kTextColor = Color(0xFF12153D);
+const kTextLightColor = Color(0xFF9A98B2);
+const kFillStarColor = Color(0xFFFCC419);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  color: Colors.black26,
+  offset: Offset(0, 4),
+  blurRadius: 4,
+);
